4f7f4b67b12bdfdb2f502fbbfdecb3c4a5c9cefd
TechConnects <techconnects@lfpl.org>
2015-01-07 16:31:12 -0500

    Create myscripts.js

---
+++ javascript/project-files/js/myscripts.js
@@ -0,0 +1,18 @@
+// function myFunction()
+// {
+// var x;
+
+// var person = prompt("Please enter your name"," ");
+
+// if (person!=null)
+//   {
+//   alert(x="Howdy " + person + "! How are you today?");
+  
+//   }
+// }
+
+var button = document.getElementById('action');
+
+button.onclick = function () {
+	alert("Hello " + prompt("What is your name?"));
+}
