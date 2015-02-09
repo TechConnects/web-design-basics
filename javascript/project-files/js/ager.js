0953c47d489087892c4dd57b2efda1fdb00986bc
TechConnects <techconnects@lfpl.org>
2015-02-09 13:06:43 -0500

    Update ager.js

--- javascript/project-files/js/ager.js
+++ javascript/project-files/js/ager.js
@@ -1,11 +1,22 @@
-var age = prompt("age?");
+//Create a variable targeting the submit button on the page using the document's getElementById method
+var submit = document.getElementById("action");
 
-var ager = function(age){
-    if(age > 13) {
-        alert("Welcome");
+//The reusable function to call on clicking the submit button
+var ager = function() {
+	//Storing age from user input
+	age = prompt("How old are you?");
+
+    //Evaluating the age variable; NOTE: currently lacking validation
+	if(age >= 13) {
+        alert("Welcome to the site");
     } else {
-        alert("not old enough");
+        alert("You're not old enough to access this site");
     }
+    
+    //logging the age value to the console
+    console.log(age);
+
 }
 
-ager(name);
+//Function call on the submit button
+submit.onclick = ager;
